00eb203f64277b63cd50e72e8cffc284527fc21f
ErikDMCosta <53874072+ErikDMCosta@users.noreply.github.com>
2024-01-02 15:36:02 -0300

    Enviando Modificações que foram Realizadas

--- 07-email-worker-compose/worker/worker.py
+++ 07-email-worker-compose/worker/worker.py
@@ -4,7 +4,8 @@ from time import sleep
 from random import randint
 
 if __name__ == '__main__':
-    r = redis.Redis(host='queue', port=6379, db=0)
+    redis_host = os.getenv('REDIS_HOST', 'queue')
+    r = redis.Redis(host=redis_host, port=6379, db=0)
     print('Aguardando a mensagens ...')
     while True:
         mensagem = json.loads(r.blpop('sender')[1])
